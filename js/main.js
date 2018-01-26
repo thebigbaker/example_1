2db6205bafd54b41abb634cd66b9a4a3dbf3681c
Jeremy Baker <30672714+thebigbaker@users.noreply.github.com>
2018-01-25 17:11:05 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  $(".button-collapse").sideNav();
+
+  /* Showcase Image Slider */
+  $('.slider').slider(indicators=false);
+
+  // Parallax
+  $('.parallax').parallax();
+
+  // Collapsible
+  $('.collapsible').collapsible();
+
+
+})
+
